d3d6f3c234e9c90733f8090bcd22340f4c2ef01f
walkingdream <walkingdream@yeah.net>
2012-09-18 21:46:38 +0800

    test add

---
+++ ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MyHelloWorld
+//
+//  Created by James on 12-9-2.
+//  Copyright (c) 2012年 James. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MyHelloWorld
+//
+//  Created by James on 12-9-2.
+//  Copyright (c) 2012年 James. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
